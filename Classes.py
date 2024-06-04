df858c1774d82c372a305b29ba1f9d8cd733fb5e
Malique <mfronche@gmail.com>
2024-06-04 12:14:40 +0200

    Added game and modified vehicle

--- Classes.py
+++ Classes.py
@@ -1,11 +1,31 @@
+import numpy as np
+
 class Vehicle():
-  def __init__(self, rotation, column, row, lenght):
+  def __init__(self, rotation, column, row, length):
     self.rotation = rotation
-    self.column = column
-    self.row = row
-    self.lenght = lenght
+    self.column = column -1
+    self.row = row -1
+    self.lenght = length
+    self.position = []
+
+    if rotation == H:
+      for _ in self.lenght:
+        self.position.append (self.row, self.column)
+        self.column += 1
+    else :
+      for _ in self.lenght:
+          self.position.append(self.row, self.column)
+          self.row +=1 
+
 
 class Board():
-  def __init__(self, size):
+  def __init__(self, size = 6):
     self.size = size
+    self.board = np.zeros((size, size))
+    print (self.board)
+
+class Game():
+  def __init__(self. )
+
+    
 
