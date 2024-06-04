c014b754717b6ea8600ae7b0a6b9ac66cea01fcd
Luc Dekker <lucdekker@Lucs-MBP.localdomain>
2024-06-04 18:34:05 +0200

    Changed variable names and added documentation

--- visualization.py
+++ visualization.py
@@ -4,18 +4,20 @@ import random
 
 
 def visualize(dictionary):
-
+    """
+    Takes dictionary containing all positions of all vehicles and plots every vehicle onto a grid.
+    """
     
-    fig, ax1 = plt.subplots(1)
-    ax1.axis([0, 6, 0, 6])
-    ax1.grid(True)
-    ax1.set_aspect('equal')
+    fig, ax = plt.subplots(1)
+    ax.axis([0, 6, 0, 6])
+    ax.grid(True)
+    ax.set_aspect('equal')
 
-    
-
+    # Loop over vehicles, assign them a color, and plot them
     for ID, value in dictionary.items():
         position = dictionary[ID].position
         
+        # Make the objective vehicle red
         if chr(ID) == 'X':
             colour = 'red'
         else: 
@@ -23,7 +25,7 @@ def visualize(dictionary):
 
         # Plot vehicles
         for tup in position:
-            ax1.plot(tup[0]+0.5, tup[1]+0.5, color=colour, marker='s', markersize=43)
+            ax.plot(tup[0]+0.5, tup[1]+0.5, color=colour, marker='s', markersize=43)
     plt.show()
    
 
