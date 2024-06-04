bf3366d4872b2434c71dcf87343b0989d3dd7186
Malique <mfronche@gmail.com>
2024-06-04 14:03:17 +0200

    makes board and places cars

--- board.py
+++ board.py
@@ -1,7 +1,26 @@
+import numpy as np
+
 class Board():
   def __init__(self, size = 6):
     self.size = size
     self.board = np.zeros((size, size))
     print (self.board)
+  
+  def place_cars(self, cars_dict):
+    self.cars = cars_dict
+    for id, vehicle in cars_dict.items():
+        location = vehicle.position
+        for tup in location:
+            self.board[tup] = vehicle.ID
+    print (self.board)
+
+  def one_move(self, ID):
+    moving_car = self.cars[ID]
+    red_front = red_car.position[-1]
+    red_back = red_car.position[0]
+
+    if 
+     
+    pass
 
     
