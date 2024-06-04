7b35b2440b2a6c3a41c2000c3ce25e478bc3e5a5
Malique <mfronche@gmail.com>
2024-06-04 12:29:40 +0200

    added game def

--- Game.py
+++ Game.py
@@ -1,2 +1,13 @@
-class Game():
+import pandas as pd
+import Classes
+import Board
+
+
+
+def game(filepath):
+    cars = pd.read_csv(filepath)
+    display (cars)
+
+    for car in cars:
+        car = Vehicle(cars[orientation, col, row, length])
     
