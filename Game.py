08b89463e129ef0571bab2d9e2ac716ea53a2960
Malique <mfronche@gmail.com>
2024-06-04 14:02:52 +0200

    Initiates game

--- Game.py
+++ Game.py
@@ -1,13 +1,32 @@
+import numpy as np
 import pandas as pd
 import Classes
-import Board
+import board
 
 
 
 def game(filepath):
     cars = pd.read_csv(filepath)
-    display (cars)
+    cars_dict = {}
+    print (cars)
 
-    for car in cars:
-        car = Vehicle(cars[orientation, col, row, length])
+    for idx, row in cars.iterrows():
+        print (row['car'])
+        ID = ord(row['car'])
+        vehicle = Classes.Vehicle(ID, row['orientation'], row['col'], row['row'], row['length'])
+        cars_dict[ID] = vehicle
+
+    game_board = board.Board()
+    game_board.place_cars(cars_dict)
+
+    game_board.one_move()
+    
+    print (game_board)
+        
+
+
+
+if __name__ == '__main__':
+    filepath = '/home/malique/Our_Rush/Rushhour6x6_1.csv'
+    game(filepath)
     
