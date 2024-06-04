e339895546ac0c29dff64d3dbc56563ab1e7f8ef
Luc Dekker <lucdekker@Lucs-MBP.localdomain>
2024-06-04 16:43:30 +0200

    Added documentation to Classes.py and Game.py, changed 'rotation' variable  into 'orientation', corrected spelling mistakes (lenght/length)

--- Classes.py
+++ Classes.py
@@ -1,20 +1,34 @@
 import numpy as np
 
+
+
 class Vehicle():
-  def __init__(self,ID, rotation, column, row, length):
+
+  # Initialise vehicle
+  def __init__(self,ID, orientation, column, row, length):
+    """
+    Instance contains the following properties
+    - ID: identification of vehicle (2 integers: XX)
+    - orientation: the vehicle is either positioned vertically (V) or horizontally (H)
+    - column: the first column of occurrence for the vehicle
+    - row: the first row of occurrence for the vehicle
+    - length: length of the vehicle, (either 2, or 3)
+    - position: list containing the position of the vehicle 
+    """
     self.ID = ID
-    self.rotation = rotation
+    self.orientation = orientation
     self.column = column -1
     self.row = row -1
-    self.lenght = length
+    self.length = length
     self.position = []
 
-    if rotation == 'H':
-      for _ in range(self.lenght):
+    # add starting positions to list
+    if orientation == 'H':
+      for _ in range(self.length):
         self.position.append ((self.row, self.column))
         self.column += 1
-    else :
-      for _ in range(self.lenght):
+    else:
+      for _ in range(self.length):
           self.position.append((self.row, self.column))
           self.row +=1 
 
--- Game.py
+++ Game.py
@@ -6,19 +6,25 @@ import board
 
 
 def game(filepath):
+    """
+    Takes a csv file containing vehicles and adds them to the game.
+    """
     cars = pd.read_csv(filepath)
     cars_dict = {}
     print (cars)
 
+    # Loop over cars dataframe, create vehicles and store them in a dictionary
     for idx, row in cars.iterrows():
         print (row['car'])
         ID = ord(row['car'])
         vehicle = Classes.Vehicle(ID, row['orientation'], row['col'], row['row'], row['length'])
         cars_dict[ID] = vehicle
 
+    # Initialise board and add cars in starting positions
     game_board = board.Board()
     game_board.place_cars(cars_dict)
 
+    # Move cars
     game_board.one_move(88)
     game_board.one_move(74)
     
