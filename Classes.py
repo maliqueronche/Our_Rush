86dc8ec68fa4d5aa47bd45ee0418e26eb481dc6b
Malique <mfronche@gmail.com>
2024-06-04 14:03:47 +0200

    added ID

--- Classes.py
+++ Classes.py
@@ -1,20 +1,21 @@
 import numpy as np
 
 class Vehicle():
-  def __init__(self, rotation, column, row, length):
+  def __init__(self,ID, rotation, column, row, length):
+    self.ID = ID
     self.rotation = rotation
     self.column = column -1
     self.row = row -1
     self.lenght = length
     self.position = []
 
-    if rotation == H:
-      for _ in self.lenght:
-        self.position.append (self.row, self.column)
+    if rotation == 'H':
+      for _ in range(self.lenght):
+        self.position.append ((self.row, self.column))
         self.column += 1
     else :
-      for _ in self.lenght:
-          self.position.append(self.row, self.column)
+      for _ in range(self.lenght):
+          self.position.append((self.row, self.column))
           self.row +=1 
 
 
