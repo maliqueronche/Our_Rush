ee239255a1552d97af198d8cf4698414ef3d6506
Noah van Wooning <noah@sp-byods-145-109-21-24.wireless.uva.nl>
2024-06-06 14:01:55 +0200

    added documentations and bug detection

--- code/board.py
+++ code/board.py
@@ -1,6 +1,13 @@
 import numpy as np
 
 class Board():
+    """
+    Board class creates board instance:
+    - Initialise board with given size
+    - Places car instances on empty playing board
+    -
+
+    """
   def __init__(self, size = 6):
     self.size = size
     self.board = np.zeros((size, size))
@@ -11,16 +18,16 @@ class Board():
     self.cars = cars_dict
 
     # Reset board for new position
-    self.board = np.zeros((self.size, self.size))
-    for id, vehicle in cars_dict.items():
-        location = vehicle.position
-        for tup in location:
-            self.board[tup] = vehicle.ID
-    self.start_board = self.board
-    print (self.board)
+    # self.board = np.zeros((self.size, self.size))
+    # for id, vehicle in cars_dict.items():
+    #     location = vehicle.position
+    #     for tup in location:
+    #         self.board[tup] = vehicle.ID
+    # self.start_board = self.board
+    # print (self.board)
 
-  # Gets the place of one space before (pos) or behind (neg) a car given an orientation (H or V)
-  def get_place(self, car_orientation, loc_tup, direction):
+  # Gets the place of one space before (pos) or behind (neg) a car given a orientation (H or V)
+  def new_position(self, car_orientation, loc_tup, direction):
 
     row, col = loc_tup
     if car_orientation == 'H':
@@ -42,44 +49,41 @@ class Board():
       return None
     return self.board[row,col]
 
-  # Does one move or logs which cars are blocking its way
+  # does one move or logs which cars are blocking it's way
   def one_move(self, ID, direction):
     moving_car = self.cars[ID]
-    print("before moving", moving_car.position)
     car_front = moving_car.position[-1]
     car_back = moving_car.position[0]
     car_orientation = moving_car.orientation
 
     if direction in ['left', 'up']:
 
-        # New position based on orientation and desired direction
-        new_back = self.get_place(car_orientation, car_back, 'back')
+        #new position based on orientation and desired direction
+        new_back = self.new_position(car_orientation, car_back, 'back')
 
-        # Check whether new back is available
+        #check whether new back is availible
         availability_back = self.check_availability(new_back)
 
 
-        # If space available, move car
+        #if availible, move car
         if availability_back == 0:
             moving_car.position.pop()
             moving_car.position.insert(0, new_back)
-      
-           
+
 
     elif direction in ['right', 'down']:
 
-        # New position based on orientation and desired direction
-        new_front = self.get_place(car_orientation, car_front, 'forward')
-        
-        # Check whether new front is available
+        #new position based on orientation and desired direction
+        new_front = self.new_position(car_orientation, car_front, 'front')
+
+        #check whether new front is availible
         availability_front = self.check_availability(new_front)
 
-        # If space available, move car
-        if availability_front == 0:
-          moving_car.position.pop(0)
-          moving_car.position.append(new_front)
-          
 
-          
+        #if availible, move car
+        if availability_front == 0:
+            print("Available")
+            moving_car.position.pop()
+            moving_car.position.insert(0, new_front)
 
     self.place_cars(self.cars)
