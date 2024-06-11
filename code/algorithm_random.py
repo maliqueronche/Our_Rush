8eb43c91f188e2fc8dbc11d700af4a6e015783c4
Malique <mfronche@gmail.com>
2024-06-11 16:03:52 +0200

    added get_random_car and is_moveable

--- code/algorithm_random.py
+++ code/algorithm_random.py
@@ -2,11 +2,42 @@ import board
 import classes
 import random
 
-# Step 1: randomly select vehicle 
-def random_algorithm(vehicles, board):
-    random_vehicle, value = random.choice(list(vehicles.items()))
-    print(random_vehicle)
+def random_step(cars_dict, board_game): #dictionary and board instance
+    car_id, car = get_random_car(cars_dict)
+    moveable = is_moveable(car)
 
-    # Step 2: check whether vehicle can make a move
     
 
+def get_random_car(car_dict):
+    print ('get random car')
+    key = random.choice(list(car_dict.keys()))
+    print (key)
+    return key, car_dict[key]
+
+def is_moveable(car):
+    positions_list = car.position
+    orientation = car.orientation
+    positions_to_check = []
+    checklist = []
+
+    if orientation == 'H':
+        left_pos = board.get_new_pos(positions_list[0], 'left')
+        positions_to_check.append(left_pos)
+        right_pos = board.get_new_pos(positions_list[-1], 'right')
+        positions_to_check.append(right_pos)
+    if orientation == 'V':
+        up_pos = board.get_new_pos(positions_list[0], 'up')
+        positions_to_check.append(up_pos)
+        down_pos = board.get_new_pos(positions_list[-1], 'right')
+        positions_to_check.append(down_pos)
+    
+    for position in positions_to_check:
+        if position.any > board.size or position.any <0:
+            checklist.append(False)
+        elif board.board[position] == 0:
+            checklist.append(True)
+        else:
+            checlist.append(False)
+
+    print (checklist)
+    return checklist
