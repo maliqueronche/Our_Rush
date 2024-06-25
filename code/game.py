0cca900dddb622b7832c68a448b749165111f115
Malique <mfronche@gmail.com>
2024-06-25 19:06:14 +0200

    little fixes

--- code/game.py
+++ code/game.py
@@ -53,53 +53,7 @@ def game(filepath, rounds, algorithm, size, hill_climb = False):
         vehicle = classes.Vehicle(ID, row['orientation'], row['col'], row['row'], row['length'])
         cars_dict[ID] = vehicle
 
-    #     # Initialise board and add cars in starting positions
-    #     game_board = board.Board(cars_dict, size)
-
-    #     if algorithm in ['random']:
-
-            
-
-    #     #     # Initiate random step
-    #     #     random_exp = ra(cars_dict, game_board)
-
-    #     #     # track current config
-    #     #     current_config = {}
-    #     #     i = 0
-
-    #     #     while cars_dict[ord('X')].position != end_position:
-
-    #     #         # for hill climb, save new board, every step
-    #     #         if hill_climb:
-    #     #             random_exp.random_step(hill_climb = True)
-    #     #             cars_dict = random_exp.cars
-    #     #             current_config[i] = random_exp.copy_cars_dict(cars_dict)
-
-    #     #             if i > min_iterations:
-    #     #                 break
-
-    #     #         else:
-    #     #             random_exp.random_step()
-                
-    #     #         i +=1
-                
-                
-    #     #     # print(f"Round {round}, Iterations: {i}, Min Iterations: {min_iterations}")
-
-    #     #     # Save the amount of iterations and use it to calculate the mean
-    #     #     iterations_list.append(i)
-    #     #     mean_i += i
-    #     #     if round % 100 == 0:
-    #     #         end = time()
-    #     #         print(f'The time elapsed: {end-start:.2f} seconds.')
-    #     #         print(f"progress:{(round/rounds) * 100}")
-    #     #         start = time()
-    #     #     round += 1
-
-    #     #     if hill_climb and i < min_iterations:
-    #     #         min_iterations = i
-    #     #         min_iterations_config = random_exp.copy_cars_dict(current_config)
-
+ 
     if algorithm == 'random':
         print(rounds)
         results = run_random(filepath, rounds, algorithm, size, end_position, hill_climb)
@@ -116,11 +70,6 @@ def game(filepath, rounds, algorithm, size, hill_climb = False):
         hc_alg = hc(filepath, end_position, size)
         results = hc_alg.run_hc()
         return results
-        
-
-
-
-        
         
 
     # Calculate the mean iterations and return the list of iterations
